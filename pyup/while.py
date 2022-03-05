9050dff449f3b046a299f64fa51bdbff8a369b0c
MinChan <76563710+kmc7582s@users.noreply.github.com>
2022-03-05 23:36:14 +0900

    Add files via upload

--- pyup/while.py
+++ pyup/while.py
@@ -21,3 +21,41 @@ while True:
     if user == 'q': # q를 입력하면 종료
         print("프로그램을 종료합니다.")
         break
+
+# 1)
+# 종료를 누를때까지 숫자들을 리스트에 담는것
+li = []
+while True:
+    user = input("그만하려면 q를 누를 입력하세요.") 
+    if user == 'q': # q를 입력하면 종료
+        print("프로그램을 종료합니다.")
+        break
+    li.append(int(user))
+# 그럼 이상 사용자가 숫자를 입력하면 list 에 추가하는 while 문을 끝냈습니다.
+print(li)
+
+# 2)
+# 문자열하고 점을 찍고 isnumeric()   > 숫자로 바꿀 수 있는 문자는 True 반환
+li = []
+while True:
+    user = input("그만하려면 q를 누를 입력하세요.") 
+    if user == 'q': # q를 입력하면 종료
+        print("프로그램을 종료합니다.")
+        break
+    if user.isnumeric():
+        li.append(int(user))
+    else:
+        print("숫자만 입력하세요!!!!")
+# 그럼 이상 사용자가 숫자를 입력하면 list 에 추가하는 while 문을 끝냈습니다.
+print(li)
+
+
+# 종료를 누를때까지 숫자들의 합을 출력
+li = []
+while True:
+    user = input("그만하려면 q를 누를 입력하세요.") 
+    if user == 'q': 
+        print("프로그램을 종료합니다.")
+        break
+    li.append(int(user))
+print(sum(li))
